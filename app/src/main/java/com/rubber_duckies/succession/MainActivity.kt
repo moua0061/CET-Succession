3e4c190de2f6597b888b9110fab3cb0bd9f803bc
Lia Moua <moua0061@yahoo.com>
2026-04-08 20:26:25 -0400

    added TODOs for next assignment/capstone

--- app/src/main/java/com/rubber_duckies/succession/MainActivity.kt
+++ app/src/main/java/com/rubber_duckies/succession/MainActivity.kt
@@ -22,5 +22,20 @@ class MainActivity : AppCompatActivity() {
             .replace(android.R.id.content, MainMenuFragment())
             .commit()
         }
+
+        //TODO: for final
+        //add more unit tests to include the scenario, options, icon, heat/loyalty/power, points, arrows following
+        //increase/deltas,
+        //I will update SRS after Jose updates the SDS
+        //add the design model to include AI in both SDS & SRS
+
+        //TODO: for the summer/capstone
+        //fix the loading when AI is thinking the next scenario
+        //fix the word text in the options to fit
+        //fix the week # to fit in the phone
+        //fix the top to not go over the date, time, wifi/data/roam heading
+        //try to give the AI a different prompt as this is a presidential campaign and we're trying to secure presidency
+        //so no blacklist
+
     }
 }
